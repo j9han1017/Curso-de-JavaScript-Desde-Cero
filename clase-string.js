70b0522b6294c1e5deef1a1127e60e92b3a1e053
j9han1017 <johandiaz101706@gmail.com>
2024-03-07 14:09:39 -0500

    clase-string

---
+++ clase-string.js
@@ -0,0 +1,9 @@
+const primeraOpcion = 'Comillas simples'
+const segundaOpcion = "Comillas simples"
+const terceraOpcion = `Comillas simples`
+
+// 1. Concatenación: Opción +   
+const direccion = 'Calle falsa 123'
+const ciudad = 'Springfield'
+const direccionCompleta = 'Mi direccion completa es' + direccion + ciudad
+console.log(direccionCompleta)
