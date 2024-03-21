b6eaaa82361a36e5627e94a3b048ca8ca0e9b1b9
j9han1017 <johandiaz101706@gmail.com>
2024-03-21 10:01:15 -0500

    ejecucion condicional

--- ejecucion-condicional-ternario.js
+++ ejecucion-condicional-ternario.js
@@ -1,2 +1,3 @@
-const edad=17;const mensaje=edad>=18?"Es mayor de edad, puede pasar":"Es menor de edad, no puede pasar";
+const edad=17;
+const mensaje=edad>=18?"Es mayor de edad, puede pasar":"Es menor de edad, no puede pasar";
 console.log(mensaje);
