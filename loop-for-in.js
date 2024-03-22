852f895b9cabc1fba88b9dfb03ad1d53b553f464
j9han1017 <johandiaz101706@gmail.com>
2024-03-22 11:28:09 -0500

    loop for in

---
+++ loop-for-in.js
@@ -0,0 +1,31 @@
+/*
+for in ---> objetos
+
+propiedades = valor
+
+array, string
+
+item
+
+for(varible in)
+
+*/
+
+const listaDeCompras = {
+    manzana: 5,
+    pera: 3,
+    naranja: 2,
+    uva: 1,
+};
+
+for (fruta in listaDeCompras) {
+    console.log(fruta)
+}
+
+for (fruta in listaDeCompras) {
+    console.log(`${fruta} : ${listaDeCompras[fruta]}`);
+}
+
+for (fruta of listaDeCompras){
+    console.log(fruta)
+}
