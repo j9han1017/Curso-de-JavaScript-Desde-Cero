435d39dcc5e0111ed31de2977df6d8b1ab7d6f46
j9han1017 <johandiaz101706@gmail.com>
2024-03-23 13:25:29 -0500

    clase de introduccion a arrays mutalidad e inmutabilidad

--- clase-introduccion-a-arrays.js
+++ clase-introduccion-a-arrays.js
@@ -44,3 +44,29 @@ console.log(firstFruit)
 
 const numberOfFruits =  fruits.length
 console.log(numberOfFruits)
+
+// Mutability
+
+fruits.push('watermelon')
+console.log(fruits)
+
+// Inmutability
+const newFruits = fruits.concat(['grape', 'kiwi'])
+console.log(fruits)
+console.log(newFruits)
+
+// Cheking arrays with Array.isArray()
+
+const isArray = Array.isArray(fruits)
+console.log(isArray)
+
+// Practical exercise: sum all elements of an array
+
+const numbersArray = [1, 2, 3, 4, 5]
+let sum = 0
+
+for (let i = 0; i < numbersArray.length; i++) {
+    sum += numbersArray[i];    
+}
+
+console.log(sum)
