481e8ab66edaca99125f85262bdf25d318c527da
j9han1017 <johandiaz101706@gmail.com>
2024-03-25 13:08:31 -0500

    clase filter reduce

---
+++ clase-filter-reduce.js
@@ -0,0 +1,18 @@
+const numbers=[1,2,3,4,5,6,7,8,9,10]
+const evenNumbers=numbers.filter(number=>number%2===0)
+console.log(numbers)
+console.log(evenNumbers)
+const numbersReduce=[1,2,3,4,5]
+const sum=numbersReduce.reduce((accumulator,currentValue)=>accumulator+currentValue,0)
+console.log(numbersReduce)
+console.log(sum)
+const words=['apple','banana','hello','bye','banana','bye','bye']
+const wordFrecuency=words.reduce((accumulator,currentValue)=>{if(accumulator[currentValue]){accumulator[currentValue]++}else{accumulator[currentValue]=1}
+return accumulator},{})
+console.log(wordFrecuency)
+const grades=[85,92,60,78,95,66,88,50,94]
+const passingGrades=grades.filter(grade=>grade>=70)
+const averagePassingGrade=passingGrades.reduce((sum,grade)=>sum+grade,0)/passingGrades.length
+console.log('Original Grades: ',grades)
+console.log('Passing Grades: ',passingGrades)
+console.log('Average Passing Grade: ',averagePassingGrade)
