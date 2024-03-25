1a6b8b37c1493f5a67d2351b3d0133bce56107a9
j9han1017 <johandiaz101706@gmail.com>
2024-03-24 19:44:31 -0500

    clase push pop

---
+++ clase-push-pop.js
@@ -0,0 +1,16 @@
+// Methods that modify the original array (Mutability)
+
+//push()
+
+const countries = ['USA', 'Canada', 'UK']
+const newCountries = countries.push('Germany', 'Australia')
+
+console.log(countries)
+console.log(newCountries)
+
+// pop
+
+const removedContry = countries.pop()
+
+console.log(countries)
+console.log(removedContry)
